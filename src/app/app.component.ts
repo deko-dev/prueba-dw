c0cda67abcfbc591a59d4e16f580acfc873d1702
Deyser Orozco <deiser1999@gmail.com>
2021-11-20 17:26:28 -0500

    :zap: [ADD] Implement Toggle for Mode Dark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prueba';
+  themeDark: boolean = false;
+  private  readonly mql = window.matchMedia('(prefers-color-scheme: dark)');
+  constructor() {
+    this.mql.matches && this.setTheme();
+  }
 
-  setTheme(theme: string){
-    window.matchMedia('(prefers-color-scheme: dark)').matches ? theme = 'dark' : theme = 'light';
-    console.log(theme);
+  ngOnInit() {
+    this.mql.onchange = () => this.setTheme();
+  }
+
+  setTheme(){
+    document.body.classList.toggle('dark');
+    this.themeDark = !this.themeDark;
+    document.querySelector('#imgTheme')?.classList.toggle('dark');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 
 // Routing Root
 import { AppRoutes } from './app.routing';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AppRoutes } from './app.routing';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(AppRoutes)
+    RouterModule.forRoot(AppRoutes),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
